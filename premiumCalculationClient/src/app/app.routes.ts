a50b83c18896e57d51026577c701ed23cb986dea
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 11:35:25 +0530

    Updates routes and config

--- premiumCalculationClient/src/app/app.routes.ts
+++ premiumCalculationClient/src/app/app.routes.ts
@@ -1,14 +1,18 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 import { PolicyComponent } from './policy/policy.component';
 import { CategoryComponent } from './category/category.component';
 import { SummaryComponent } from './summary/summary.component';
 import { RatesComponent } from './rates/rates.component';
-import { DiscountComponent } from './discount/discount.component';
+import { DiscountsComponent } from './discount/discount.component';
 
 export const routes: Routes = [
   { path: '', component: PolicyComponent },
   { path: 'category', component: CategoryComponent },
   { path: 'summary', component: SummaryComponent },
   { path: 'rates', component: RatesComponent },
-  { path: 'discount', component: DiscountComponent },
+  { path: 'discount', component: DiscountsComponent },
+];
+
+export const appRouterProviders = [
+  provideRouter(routes)
 ];
