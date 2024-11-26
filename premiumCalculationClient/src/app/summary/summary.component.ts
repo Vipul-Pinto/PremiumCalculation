7c64b9ebffce94fbdffaf2841f30cfdfa365ae6e
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 14:43:12 +0530

    updated logic for premiumCalculation

--- premiumCalculationClient/src/app/summary/summary.component.ts
+++ premiumCalculationClient/src/app/summary/summary.component.ts
@@ -1,9 +1,68 @@
+// import { NgFor, NgIf } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { environment } from '../../environments/environment';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-summary',
+//   imports: [NgFor, NgIf],
+//   templateUrl: './summary.component.html',
+//   styleUrl: './summary.component.css',
+// })
+// export class SummaryComponent implements OnInit {
+//   completePolicyData: any;
+//   calculatedPremium: any | null = null;
+//   rates: any;
+
+//   // constructor(private http: HttpClient) {
+    
+//   // }
+//   constructor(
+//     private router: Router,
+//     //private fb: FormBuilder,
+//     private http: HttpClient
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.completePolicyData = history.state.policyData;
+//     this.http
+//       .get(`${environment.apiUrl}/rates`)
+//       .subscribe((res) => console.log(res));
+//   }
+
+//   calculatePremium(schemeNumber: any) {
+//     this.http
+//       .get(`${environment.apiUrl}/summary/${schemeNumber}`)
+//       .subscribe((res) => {
+//         this.calculatedPremium = res.toString();
+//         console.log(typeof this.calculatedPremium);
+//       });
+//     }
+//   goBack() {
+//     this.router.navigate(['/category'], {
+//       state: {
+//        // policyData: this.policyData,
+//         // categoryData: this.categoryForm.value,
+//       },
+//     });
+//   }
+//   goNext() {
+//     this.router.navigate(['/rates'], {
+//       state: {
+//        // policyData: this.policyData,
+//         // categoryData: this.categoryForm.value,
+//       },
+//     });
+//   }
+
+  
+// }
 import { NgFor, NgIf } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Router } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-summary',
   imports: [NgFor, NgIf],
@@ -12,44 +71,26 @@ import { Router } from '@angular/router';
 })
 export class SummaryComponent implements OnInit {
   completePolicyData: any;
-  calculatedPremium: any | null = null;
+  calculatedPremium: string;
   rates: any;
-
-  // constructor(private http: HttpClient) {
-    
-  // }
-  constructor(
-    private router: Router,
-    //private fb: FormBuilder,
-    private http: HttpClient
-  ) {}
-
+ 
+  constructor(private http: HttpClient) {
+    this.calculatedPremium = '';
+  }
+ 
   ngOnInit(): void {
     this.completePolicyData = history.state.policyData;
     this.http
       .get(`${environment.apiUrl}/rates`)
       .subscribe((res) => console.log(res));
   }
-
-  calculatePremium() {
-    
+ 
+  calculatePremium(schemeNumber: any) {
+    this.http
+      .get(`${environment.apiUrl}/summary/${schemeNumber}`)
+      .subscribe((res) => {
+        this.calculatedPremium = res.toString();
+        console.log(typeof this.calculatedPremium);
+      });
   }
-  goBack() {
-    this.router.navigate(['/category'], {
-      state: {
-       // policyData: this.policyData,
-        // categoryData: this.categoryForm.value,
-      },
-    });
-  }
-  goNext() {
-    this.router.navigate(['/rates'], {
-      state: {
-       // policyData: this.policyData,
-        // categoryData: this.categoryForm.value,
-      },
-    });
-  }
-
-  
 }
