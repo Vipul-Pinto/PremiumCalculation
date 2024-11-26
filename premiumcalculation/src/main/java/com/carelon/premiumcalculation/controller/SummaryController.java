e1c159467baef7818ca2175109ec60f337f34cc5
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 11:00:14 +0530

    Added summary controller

---
+++ premiumcalculation/src/main/java/com/carelon/premiumcalculation/controller/SummaryController.java
@@ -0,0 +1,23 @@
+package com.carelon.premiumcalculation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carelon.premiumcalculation.service.SummaryService;
+
+import lombok.Data;
+
+@RestController
+@RequestMapping("/summary")
+@Data
+public class SummaryController {
+	
+	private final SummaryService summaryService;
+
+	@GetMapping("/{schemeNumber}")
+	public float getPremium(@PathVariable String schemeNumber) {
+		return summaryService.premiumCalculation(schemeNumber);
+	}
+}
