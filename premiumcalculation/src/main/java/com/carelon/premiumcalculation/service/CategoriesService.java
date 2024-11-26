1db93c219c61a30de93fc0c6e03e5acf24356dbc
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 11:04:52 +0530

    Update CategoriesService.java

--- premiumcalculation/src/main/java/com/carelon/premiumcalculation/service/CategoriesService.java
+++ premiumcalculation/src/main/java/com/carelon/premiumcalculation/service/CategoriesService.java
@@ -8,11 +8,16 @@ import com.carelon.premiumcalculation.entity.CategoriesEntity;
 import com.carelon.premiumcalculation.entity.PoliciesEntity;
 import com.carelon.premiumcalculation.repository.CategoriesRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.Data;
 
 @Service
 @Data
 public class CategoriesService {
+
+    @PersistenceContext
+	private EntityManager entityManager;
 
     private final CategoriesRepository categoriesRepository;
     private final ModelMapper mapper;
@@ -23,6 +28,14 @@ public class CategoriesService {
         categoriesEntity.setPcPolicies(policiesEntity);
         return mapper.map(categoriesRepository.save(categoriesEntity), Categories.class);
     }
+
+    public List<Categories> getCategoryById(String schemeNumber) {
+		List<CategoriesEntity> categoriesEntity = entityManager
+				.createNamedQuery("getCategoryByScheme", CategoriesEntity.class)
+				.setParameter("schemeNumber", schemeNumber).getResultList();
+		return categoriesEntity.stream().map(category -> mapper.map(category, Categories.class))
+				.collect(Collectors.toList());
+	}
 
     public void deleteCategory(Integer categoryId) {
         categoriesRepository.deleteById(categoryId);
