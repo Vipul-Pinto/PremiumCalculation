bab6f4c1517b542420cf3bc3a621404a67506967
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 11:34:25 +0530

    Updated app component

--- premiumCalculationClient/src/app/app.component.ts
+++ premiumCalculationClient/src/app/app.component.ts
@@ -1,12 +1,32 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'premiumCalculationClient';
+// }
+import { NgClass, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ROUTER_CONFIGURATION, ROUTES, RouterLink, RouterOutlet, withRouterConfig } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'premiumCalculationClient';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  selectedTab: string = 'policyInfo';
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
 }
