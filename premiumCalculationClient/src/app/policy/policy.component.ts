44b1e58eedf8bcad1560a36a9ac8e216ee6d363d
Vipul Joseph Pinto <36556118+Vipul-Pinto@users.noreply.github.com>
2024-11-26 11:32:14 +0530

    Updated policy component

--- premiumCalculationClient/src/app/policy/policy.component.ts
+++ premiumCalculationClient/src/app/policy/policy.component.ts
@@ -1,54 +1,75 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-policy',
-  imports: [ReactiveFormsModule],
   templateUrl: './policy.component.html',
-  styleUrl: './policy.component.css',
+  styleUrls: ['./policy.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
 export class PolicyComponent implements OnInit {
   policyForm!: FormGroup;
   policyData: any;
   categoryData: any;
+
   constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     this.policyForm = this.fb.group({
-      schemeName: [''],
-      schemeNumber: [''],
-      premium: [0],
-      state: [''],
-      effectiveDate: [''],
-      anniversary: [''],
-      businessSource: [''],
-      taxId: [''],
-      country: [''],
-      employerContractInfo: [''],
-      address1: [''],
+      schemeName: ['', Validators.required],
+      schemeNumber: ['', Validators.required],
+      premium: [0, [Validators.required, Validators.min(0)]],
+      state: ['', Validators.required],
+      effectiveDate: ['', Validators.required],
+      anniversary: ['', Validators.required],
+      businessSource: ['', Validators.required],
+      taxId: ['', Validators.required],
+      country: ['', Validators.required],
+      employerContractInfo: ['', Validators.required],
+      address1: ['', Validators.required],
       address2: [''],
       address3: [''],
-      city: [''],
-      zip: [''],
-      employerPhone: [''],
+      city: ['', Validators.required],
+      zip: ['', Validators.required],
+      employerPhone: ['', Validators.required],
       employerExt: [''],
-      employerFax: [''],
+      employerFax: ['']
     });
+
+   
     this.policyData = history.state.policyData;
     if (this.policyData) {
       this.policyForm.patchValue(this.policyData);
     }
     this.categoryData = history.state.categoryData;
+
+    
+    interval(30000).subscribe(() => {
+      this.autoSave();
+    });
   }
 
   onSubmit(): void {
-    this.router.navigate(['/category'], {
-      state: {
-        policyData: this.policyForm.value,
-        categoryData: this.categoryData,
-      },
-    });
+    console.log(this.policyForm.valid);
+    if (this.policyForm.valid) {
+      this.router.navigate(['/category'], {
+        state: {
+          policyData: this.policyForm.value,
+          categoryData: this.categoryData,
+        },
+      });
+    } else {
+      this.policyForm.markAllAsTouched(); 
+    }
+  }
+
+  autoSave(): void {
+    // Implement the auto-save functionality (e.g., save to localStorage)
+    localStorage.setItem('policyData', JSON.stringify(this.policyForm.value));
+    console.log('Form data auto-saved');
   }
 }
